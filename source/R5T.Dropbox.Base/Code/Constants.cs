af0a995b3f6e2f463a97d6c0a6855c4238f57873
MinexAutomation <minex.automation@gmail.com>
2019-09-01 19:39:49 -0400

    Added Dropbox directory path functionality, both stringly- and strongly-typed.

---
+++ source/R5T.Dropbox.Base/Code/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+using R5T.NetStandard.IO.Paths.Extensions;
+
+
+namespace R5T.Dropbox.Base
+{
+    public static class Constants
+    {
+        public const string DropboxDirectoryNameValue = @"Dropbox";
+        public static DirectoryName DropboxDirectoryName = Constants.DropboxDirectoryNameValue.AsDirectoryName();
+    }
+}
---
+++ source/R5T.Dropbox.Base/Code/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+using R5T.NetStandard.IO.Paths.Extensions;
+
+using BasePathUtilities = R5T.NetStandard.IO.Paths.Base.Utilities;
+using PathUtilities = R5T.NetStandard.IO.Paths.Utilities;
+
+
+namespace R5T.Dropbox.Base
+{
+    public static class Utilities
+    {
+        public static string DropboxDirectoryPathValue
+        {
+            get
+            {
+                var output = PathUtilities.Combine(BasePathUtilities.UserProfileDirectoryPathValue, Constants.DropboxDirectoryNameValue);
+                return output;
+            }
+        }
+
+        public static DirectoryPath DropboxDirectoryPath
+        {
+            get
+            {
+                var output = Utilities.DropboxDirectoryPathValue.AsDirectoryPath();
+                return output;
+            }
+        }
+    }
+}
